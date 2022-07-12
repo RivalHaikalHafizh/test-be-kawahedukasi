17af78adc0099a1a55eca78bfae6aefbe1475e9f
HaikalHafizh <rivalhh130702@gmail.com>
2022-07-12 15:49:14 +0700

    Nama:rivalhaikalhafizh

--- test_soal2.php
+++ test_soal2.php
@@ -3,11 +3,11 @@
 
 for($i =0;$i<count($data);$i++){
 if($i <0){
-	echo "Tidak bisa di validasi sistem\n";
+	print("Tidak bisa di validasi sistem<br>");
 }elseif(is_string($data[$i])){
-	echo "index ke $i adalah integer dengan data $data[$i]\n";
+	print("-index ke $i adalah integer dengan data $data[$i] <br>");
 }elseif(is_int($data[$i])){
-	echo "index ke $i adalah string dengan data $data[$i]\n";
+	print("-index ke $i adalah string dengan data $data[$i]<br>");
 }
 }
 ?>
