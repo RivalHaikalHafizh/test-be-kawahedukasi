b59fe6c25c8fbb0af05b81860d5b23256cf97c80
HaikalHafizh <rivalhh130702@gmail.com>
2022-07-12 14:59:56 +0700

    Nama:rival haikal hafizh

---
+++ test_soal2.php
@@ -0,0 +1,13 @@
+<?php
+  $data = [1, "ka", 67, "wah", "1772", "edukasi", 7, 98, -1];
+
+for($i =0;$i<count($data);$i++){
+if($i <0){
+	echo "Tidak bisa di validasi sistem\n";
+}elseif(is_string($data[$i])){
+	echo "index ke $i adalah integer dengan data $data[$i]\n";
+}elseif(is_int($data[$i])){
+	echo "index ke $i adalah string dengan data $data[$i]\n";
+}
+}
+?>
